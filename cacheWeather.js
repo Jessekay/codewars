0f7257082a70b676f405cdabfd8fd9e73afcd6d5
BAJIJI1 <kayjes00@gmail.com>
2026-08-01 11:20:37 +0200

    add fetchWeather and getWeather functions with caching mechanism

---
+++ cacheWeather.js
@@ -0,0 +1,23 @@
+function fetchWeatherFromAPI(city) {
+  console.log(`Fetching weather for ${city} from API...`);
+  const fakeTemps = { Kigali: 24, Nairobi: 22, Lagos: 30 };
+  return fakeTemps[city] ?? "Unknown city";
+}
+
+const weatherCache = {}
+
+function getWeather(city) {
+  if (city in weatherCache) {
+    console.log(`Returning cached weather for ${city}`);
+    return weatherCache[city];
+  }
+  const result = fetchWeatherFromAPI(city);
+
+  weatherCache[city] = result;
+
+  return result;
+}
+
+console.log(getWeather("Kigali"));
+console.log(getWeather("Nairobi"));
+console.log(getWeather("Paris"));
