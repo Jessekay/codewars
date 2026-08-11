865f22f24e52d68827c7b8d88b5b6a1c1a9f5ed6
BAJIJI1 <kayjes00@gmail.com>
2026-08-11 13:52:53 +0200

    last function to use rest parameters and improve argument handling

--- last.js
+++ last.js
@@ -1,12 +1,29 @@
-function last() {
-  if (arguments.length > 1) {
-   return arguments[arguments - 1]; 
-  }
+// function last() {
+//   if (arguments.length > 1) {
+//    return arguments[arguments - 1]; 
+//   }
   
-  const arg = arguments[0];
+//   const arg = arguments[0];
   
-  if (Array.isArray(arg) || typeof arg === 'string') {
-    return arg[arg.length - 1];
+//   if (Array.isArray(arg) || typeof arg === 'string') {
+//     return arg[arg.length - 1];
+//   }
+//   return arg;
+// }
+
+function last(...args) {
+
+  if (args.length === 1) {
+    const singleArg = args[0];
+
+
+    if (Array.isArray(singleArg) || typeof singleArg === 'string') {
+      return singleArg[singleArg.length - 1];
+    }
+
+
+    return singleArg;
   }
-  return arg;
+
+  return args[args.length - 1];
 }
